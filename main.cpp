c08e038eaaa4af6e7fe59040e22d141b9ba73bb7
Tamik13 <tamiklalis@gmail.com>
2026-08-21 20:31:26 +0300

    Add green text

--- main.cpp
+++ main.cpp
@@ -6,8 +6,12 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define RED_TEXT_START "\033[91m"
-#define RED_TEXT_END "\033[0m"
+#define COLOR_TEXT(STR, COLOR) "\033[" COLOR "m" STR "\033[0m"
+
+#define RED "91"
+#define GREEN "92"
+#define BLUE "94"
+
 #define DELTA 1e-8
 
 enum Errors {
@@ -64,6 +68,8 @@ Errors read_input(double* const pt_coef_a, double* const pt_coef_b, double* cons
 
     read_one(pt_coef_a, "first");
 
+    printf("%lg\n", *pt_coef_a);
+
     read_one(pt_coef_b, "second");
 
     read_one(pt_coef_c, "thread");
@@ -82,11 +88,10 @@ Errors read_one(double* const pt_coef, const char* str_num) {
             clean_buffer();
         }
 
-        printf(RED_TEXT_START
-               "Incorrect input\n"
-               "Try again\n"
-               RED_TEXT_END
-               "Enter %s coefficient: ", str_num);
+        printf(COLOR_TEXT(
+                "Incorrect input\n"
+                "Try again\n", RED)
+                "Enter %s coefficient: ", str_num);
     }
 
     return SUCCESS;
@@ -170,28 +175,24 @@ void print_output(Solutions count_sol, const double first_sol, const double seco
 
     switch (count_sol) {
         case NO_SOLUTION:
-            printf("No solution\n");
+            printf(COLOR_TEXT("No solution\n", GREEN));
             break;
         case ONE_SOLUTION:
-            printf("One solution: %lg\n", first_sol);
+            printf(COLOR_TEXT("One solution: %lg\n", GREEN), first_sol);
             break;
         case TWO_SOLUTION:
-            printf("Two solution: %lg %lg\n", first_sol, second_sol);
+            printf(COLOR_TEXT("Two solution: %lg %lg\n", GREEN), first_sol, second_sol);
             break;
         case INFINITE_SOLUTION:
-            printf("Infinite solution\n");
+            printf(COLOR_TEXT("Infinite solution\n", GREEN));
             break;
         case INIT_VALUE:
-            printf(RED_TEXT_START "INIT VALUE IN COUNT_SOL ERROR\n" RED_TEXT_END);
+            printf(COLOR_TEXT("INIT VALUE IN COUNT_SOL ERROR\n", RED));
             return;
         default:
-            printf(RED_TEXT_START "SWITCH ERROR\n" RED_TEXT_END);
+            printf(COLOR_TEXT("SWITCH ERROR\n", RED));
             return;
     }
 
     return;
 }
-
-/*
--D _DEBUG -ggdb3 -std=c++17 -Wall -Wextra -Weffc++ -Wc++14-compat -Wmissing-declarations -Wcast-align -Wcast-qual -Wchar-subscripts -Wconversion -Wctor-dtor-privacy -Wempty-body -Wfloat-equal -Wformat-nonliteral -Wformat-security -Wformat-signedness -Wformat=2 -Winline -Wnon-virtual-dtor -Woverloaded-virtual -Wpacked -Wpointer-arith -Winit-self -Wredundant-decls -Wshadow -Wsign-conversion -Wsign-promo -Wstrict-overflow=2 -Wsuggest-override -Wswitch-default -Wswitch-enum -Wundef -Wunreachable-code -Wunused -Wvariadic-macros -Wno-missing-field-initializers -Wno-narrowing -Wno-old-style-cast -Wno-varargs -Wstack-protector -fcheck-new -fsized-deallocation -fstack-protector -fstrict-overflow -fno-omit-frame-pointer -Wlarger-than=8192 -fPIE -Werror=vla -fsanitize=address,alignment,bool,bounds,enum,float-cast-overflow,float-divide-by-zero,integer-divide-by-zero,nonnull-attribute,null,return,returns-nonnull-attribute,shift,signed-integer-overflow,undefined,unreachable,vla-bound,vptr
-*/
