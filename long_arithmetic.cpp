b7b6f010072b8a4af4cf90b10c793c0981cef49c
Tamik13 <tamiklalis@gmail.com>
2026-08-29 16:02:43 +0300

    add print long_arithmetic_int

--- long_arithmetic.cpp
+++ long_arithmetic.cpp
@@ -2,13 +2,14 @@
 #include <stdbool.h>
 #include <limits.h>
 #include <assert.h>
+#include <ctype.h>
 
 #define ASSERT_FOR_ARR(ind, size)     \
     assert(0 <= ind && i < size)
 
 #define LONG_ARTHM_LEN 100
 #define BLOCK_MAX 999999999
-#define COUNT_DIGIT_IN_BLOCK "9"
+#define COUNT_DIGIT_IN_BLOCK 9
 
 
 struct long_arithmetic_int {
@@ -16,21 +17,26 @@ struct long_arithmetic_int {
 };
 
 void int_to_long_arithmetic_int(const unsigned int num, long_arithmetic_int* const lng_num);
-void print_long_arithmetic_int(const long_arithmetic_int* const lng_num);
 
-long_arithmetic_int sum_long_arithmetic_int(const long_arithmetic_int* const lng_num1, const long_arithmetic_int* const lng_num2);
+long_arithmetic_int sum_long_arithmetic_int           (const long_arithmetic_int* const lng_num1, const long_arithmetic_int* const lng_num2);
 long_arithmetic_int multiplication_long_arithmetic_int(const long_arithmetic_int* const lng_num1, const long_arithmetic_int* const lng_num2);
+void assigning_long_arithmetic_int(long_arithmetic_int* const lhs_lng_num, long_arithmetic_int* rhs_lng_num);
+void reset_to_zero_long_arithmetic_int(long_arithmetic_int* const lng_num);
+
+void read_long_arithmetic_int(long_arithmetic_int* const lng_int);
+void print_long_arithmetic_int (const long_arithmetic_int* const lng_num);
+
 
 
 int main() {
     long_arithmetic_int num1, num2;
 
-    int_to_long_arithmetic_int(BLOCK_MAX, &num1);
-    int_to_long_arithmetic_int(BLOCK_MAX, &num2);
+    read_long_arithmetic_int(&num1);
+//     int_to_long_arithmetic_int(BLOCK_MAX, &num2);
+//
+//     long_arithmetic_int new_num = multiplication_long_arithmetic_int(&num1, &num2);
 
-    long_arithmetic_int new_num = multiplication_long_arithmetic_int(&num1, &num2);
-
-    print_long_arithmetic_int(&new_num);
+    print_long_arithmetic_int(&num1);
 
     return 0;
 }
@@ -88,6 +94,26 @@ long_arithmetic_int multiplication_long_arithmetic_int(const long_arithmetic_int
 }
 
 
+void assigning_long_arithmetic_int(long_arithmetic_int* const lhs_lng_num, long_arithmetic_int* rhs_lng_num) {
+    assert(lhs_lng_num != NULL);
+    assert(rhs_lng_num != NULL);
+
+    for (int i = 0; i < LONG_ARTHM_LEN; i++) {
+        lhs_lng_num->arr[i] = rhs_lng_num->arr[i];
+    }
+}
+
+
+void reset_to_zero_long_arithmetic_int(long_arithmetic_int* const lng_num) {
+    assert(lng_num != NULL);
+
+    long_arithmetic_int zero_num;
+
+    assigning_long_arithmetic_int(lng_num, &zero_num);
+}
+
+
+
 void print_long_arithmetic_int(const long_arithmetic_int* const lng_num) {
     assert(lng_num != NULL);
 
@@ -99,8 +125,43 @@ void print_long_arithmetic_int(const long_arithmetic_int* const lng_num) {
     i--;
 
     for (; i >= 0; i--) {
-        printf("%0" COUNT_DIGIT_IN_BLOCK "u", lng_num->arr[i]);
+        printf("%09u", lng_num->arr[i]);
     }
 }
+
+
+void read_long_arithmetic_int(long_arithmetic_int* const lng_num) {
+    assert(lng_num != 0);
+
+    char symbol = '\0';
+    char str_num[LONG_ARTHM_LEN * COUNT_DIGIT_IN_BLOCK + 1];
+    size_t len_str_num = 0;
+    int digit = 0;
+
+    reset_to_zero_long_arithmetic_int(lng_num);
+
+    while (isspace(symbol = getchar()) || symbol == '0') {
+        continue;
+    }
+
+    str_num[len_str_num++] = symbol;
+
+    while ('0' <= (symbol = getchar()) && symbol <= '9' && len_str_num < LONG_ARTHM_LEN * COUNT_DIGIT_IN_BLOCK) {
+        str_num[len_str_num++] = symbol;
+    }
+    str_num[len_str_num--] = '\0';
+
+    for (int i = len_str_num; i >= 0;) {
+        unsigned int degree_of_ten = 1;
+
+        for (int j = 0; j < COUNT_DIGIT_IN_BLOCK && i >= 0; j++, i--) {
+            lng_num->arr[digit] += (str_num[i] - '0') * degree_of_ten;
+            degree_of_ten *= 10;
+        }
+
+        digit++;
+    }
+}
+
 
 
